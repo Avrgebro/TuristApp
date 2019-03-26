6f7b0bd74a981fc840f0a36ca027ce2b4b15007a
Avrgebro <jabcarranz@gmail.com>
2019-03-25 19:13:22 -0500

    Listview added

---
+++ app/src/main/java/com/turistapp/jose/turistapp/Adapters/PlacesListAdapter.java
@@ -0,0 +1,93 @@
+package com.turistapp.jose.turistapp.Adapters;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.chauthai.swipereveallayout.SwipeRevealLayout;
+import com.chauthai.swipereveallayout.ViewBinderHelper;
+import com.turistapp.jose.turistapp.R;
+
+import java.util.List;
+
+public class PlacesListAdapter extends ArrayAdapter<String> {
+
+    private final LayoutInflater mInflater;
+    private final ViewBinderHelper binderHelper;
+
+    public PlacesListAdapter(Context context, List<String> objects) {
+        super(context, R.layout.places_item, objects);
+        mInflater = LayoutInflater.from(context);
+        binderHelper = new ViewBinderHelper();
+
+        // uncomment if you want to open only one row at a time
+        // binderHelper.setOpenOnlyOne(true);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.places_item, parent, false);
+
+            holder = new ViewHolder();
+            holder.swipeLayout = (SwipeRevealLayout) convertView.findViewById(R.id.swipe_layout);
+            holder.frontView = convertView.findViewById(R.id.front_layout);
+            holder.deleteView = convertView.findViewById(R.id.delete_layout);
+            holder.textView = (TextView) convertView.findViewById(R.id.text);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        final String item = getItem(position);
+        if (item != null) {
+            binderHelper.bind(holder.swipeLayout, item);
+
+            holder.textView.setText(item);
+            holder.deleteView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    remove(item);
+                }
+            });
+            holder.frontView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String displayText = "" + item + " clicked";
+                }
+            });
+        }
+
+        return convertView;
+    }
+
+    /**
+     * Only if you need to restore open/close state when the orientation is changed.
+     * Call this method in {@link android.app.Activity#onSaveInstanceState(Bundle)}
+     */
+    public void saveStates(Bundle outState) {
+        binderHelper.saveStates(outState);
+    }
+
+    /**
+     * Only if you need to restore open/close state when the orientation is changed.
+     * Call this method in {@link android.app.Activity#onRestoreInstanceState(Bundle)}
+     */
+    public void restoreStates(Bundle inState) {
+        binderHelper.restoreStates(inState);
+    }
+
+    private class ViewHolder {
+        SwipeRevealLayout swipeLayout;
+        View frontView;
+        View deleteView;
+        TextView textView;
+    }
+}
